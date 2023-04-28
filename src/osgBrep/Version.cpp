a0ab8602e8b751ef62eeb359cf6350a1c621ab1c
rmartino <martinorocco@gmail.com>
2023-04-28 15:00:41 +0200

    added Brep project

---
+++ src/osgBrep/Version.cpp
@@ -0,0 +1,30 @@
+/* -*-c++-*- osgBrep - Copyright (C) 2023 Rocco Martino
+ *
+ * This library is open source and may be redistributed and/or modified under
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * OpenSceneGraph Public License for more details.
+*/
+
+#include <osgBrep/Version>
+#include <osg/Version>
+
+extern "C" {
+
+const char* osgBrepGetVersion()
+{
+    return osgGetVersion();
+}
+
+
+const char* osgBrepGetLibraryName()
+{
+    return "OpenSceneGraph BRep Library";
+}
+
+}
